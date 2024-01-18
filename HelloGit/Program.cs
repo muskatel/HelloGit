786a14543294b763241422e86f9b4a20c32148b2
Craig Marais <craig.marais.85@gmail.com>
2024-01-18 09:03:37 +0100

    Added CountTo()

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -6,5 +6,16 @@ class Program
     {
         Console.WriteLine("Hello, Git!");
         Console.WriteLine("My name is Craig");
+        
+        CountTo(5);
+    }
+
+    static void CountTo(int num)
+    {
+        Console.WriteLine($"Counting to {num}");
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine( i + " ...");
+        }
     }
 }
